7faf48bc60c8f20df6ca92c17c322166488d6b5c
Administrator <Administrator@SD-20181229CKLG>
2019-02-25 14:31:29 +0800

    动态路由

---
+++ src/main/java/com/example/demo/ApiGetwayDynamicRouteApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGetwayDynamicRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetwayDynamicRouteApplication.class, args);
+	}
+	
+	@Bean
+	@RefreshScope
+	@ConfigurationProperties("zuul")
+	public ZuulProperties zuulProperties() {
+		return new ZuulProperties();
+	}
+
+}
